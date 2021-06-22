0dd75b8c0ce0a2664aaa93a9a127f2a15e8288f6
mehdi halfya <halfyamehdi@gmail.com>
2021-06-22 16:01:54 +0200

    first commitdxd

--- src/components/blcok_from.vue
+++ src/components/blcok_from.vue
@@ -4,24 +4,24 @@
 
     <div class="from__block">
 
-	<form  action="http://localhost/php/brojetEmail/index.php" methods="POST">
+	<form  action="http://localhost/php/brojetEmail/test.php" methods="POST">
 
 		<div class="form-group">
 			<label for="exampleInputEmail1">Email address</label>
 			<input type="email" name="email" class="form-control" aria-describedby="emailHelp"
-				placeholder="Enter email">
+			required	placeholder="Enter email"> 
 		</div>
 		<div class="form-group">
 			<label for="exampleInputPassword1">Name </label>
-			<input type="text" class="form-control" name="entreprise" placeholder="Name ">
+			<input type="text" class="form-control" name="entreprise"  required placeholder="Name entrerise">
 		</div>
 		<div class="form-group">
 			<label for="exampleInputPassword1">numéro de téléphone</label>
-			<input type="text" class="form-control" name="telephone" placeholder="numéro de téléphone">
+			<input type="text" class="form-control" name="telephone" required placeholder="numéro de téléphone">
 		</div>
 		<div class="form-group">
 			<label for="exampleInputPassword1">Commentaires</label>
-			<textarea class="form-control" rows="5" name="Commentaires">Commentaires</textarea>
+			<textarea class="form-control" rows="5" required  placeholder="Commentairess" name="Commentaires"></textarea>
 		</div>
 		<button type="submit" class="btn btn-primary">Submit</button>
 	</form>
